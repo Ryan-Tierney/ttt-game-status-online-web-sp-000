383e94dbb5b523409595ace98f421c1ba8a52257
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-12-31 16:19:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,10 +20,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
-    #check for player 1 win
     if check_win_combination?(board, 'X', win_combo)
       return win_combo
-      #check for player 2 win
     elsif check_win_combination?(board, 'O', win_combo)
       return win_combo
     else
