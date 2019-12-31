172c68af240f289fb7b1e2ef34c4abcdf1df1615
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-12-31 16:31:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,9 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board) || won?(board)
+  elsif !won?(board) && !full?(board)
+    return false 
+  elsif won?(board)
     return false 
   end
 end
